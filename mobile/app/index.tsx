88fba0965d23c12445d9d0f995de640287a7491a
Vickey Kumar <kumar21vickey@gmail.com>
2025-07-09 18:46:53 +0530

    auth added in mobile folder

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,11 +1,14 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+ 
+const HomeScreen = () => {
   return (
-     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-green-500">
-        Welcome to Nativewind!
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Hello
       </Text>
     </View>
   );
 }
+
+export default HomeScreen;
