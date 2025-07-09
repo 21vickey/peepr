894b46ded68d403682a67be3e76f1e3732139094
Vickey Kumar <kumar21vickey@gmail.com>
2025-07-09 21:23:02 +0530

    tabs setup done and signout added

---
+++ mobile/app/(tabs)/search.tsx
@@ -0,0 +1,12 @@
+import { View, Text} from "react-native";
+
+const SearchScreen = () => {
+  return (
+    <View className="flex-1 bg-white px-4">
+      <Text className="text-black text-lg font-semibold mt-4">Search</Text>
+      {/* Add your search functionality here */}
+    </View>
+  );
+};
+
+export default SearchScreen;
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -7,7 +7,9 @@ export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
       <Stack>
+        
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
     </ClerkProvider>
   );
