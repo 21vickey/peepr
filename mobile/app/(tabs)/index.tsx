fbd7cf5fc671d302f2bfc4de8692dc21b81844c5
Vickey Kumar <kumar21vickey@gmail.com>
2025-07-12 07:32:54 +0530

    solved user error form server problem from arcjet solved i am remove arcjet everything and added post like detete

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,8 +1,14 @@
+import PostComposer from "@/components/PostComposer";
+import PostsList from "@/components/PostsList";
 import SignOutButton from "@/components/SignOutButton";
 import { useUserSync } from "@/hooks/useUserSync";
 import { Ionicons } from "@expo/vector-icons";
-import { View, Text} from "react-native";
-import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
+import React from "react";
+import { View, Text, ScrollView } from "react-native";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
 const HomeScreen = () => {
   useUserSync(); // Sync user data on mount
@@ -10,14 +16,23 @@ const HomeScreen = () => {
   const insets = useSafeAreaInsets();
 
   return (
-    <SafeAreaView className="flex-1 bg-white" style={{ paddingTop: insets.top }}>
-     <View className="flex-row justify-between items-center px-4 py-3 border-b border-gray-100">
+    <SafeAreaView
+      className="flex-1 bg-white"
+      // style={{ paddingTop: insets.top }}
+    >
+      <View className="flex-row justify-between items-center px-4 py-3 border-b border-gray-100">
         <Ionicons name="logo-twitter" size={24} color="#1DA1F2" />
         <Text className="text-xl font-bold text-gray-900">Home</Text>
         <SignOutButton />
       </View>
-
       
+      <ScrollView>
+        <View style={{ backgroundColor: "white", flex: 1 }}>
+          <PostComposer />
+        </View>
+
+        <PostsList />
+      </ScrollView>
     </SafeAreaView>
   );
 };
