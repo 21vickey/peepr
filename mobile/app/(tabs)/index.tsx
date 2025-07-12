961240e5f6f08918af756253aebbbb1893c0267a
Vickey Kumar <kumar21vickey@gmail.com>
2025-07-12 19:53:15 +0530

    notification added

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,16 +1,28 @@
 import PostComposer from "@/components/PostComposer";
 import PostsList from "@/components/PostsList";
 import SignOutButton from "@/components/SignOutButton";
+import { usePosts } from "@/hooks/usePosts";
 import { useUserSync } from "@/hooks/useUserSync";
 import { Ionicons } from "@expo/vector-icons";
-import React from "react";
-import { View, Text, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { View, Text, ScrollView, RefreshControl } from "react-native";
 import {
   SafeAreaView,
   useSafeAreaInsets,
 } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
+
+  const [isRefetching, setIsRefetching] = useState(false);
+  const { refetch: refetchPosts } = usePosts();
+
+  const handlePullToRefresh = async () => {
+    setIsRefetching(true);
+
+    await refetchPosts();
+    setIsRefetching(false);
+  };
+
   useUserSync(); // Sync user data on mount
 
   const insets = useSafeAreaInsets();
@@ -18,7 +30,8 @@ const HomeScreen = () => {
   return (
     <SafeAreaView
       className="flex-1 bg-white"
-      // style={{ paddingTop: insets.top }}
+      edges={["top"]}
+      // style={{ edge: insets.top }}
     >
       <View className="flex-row justify-between items-center px-4 py-3 border-b border-gray-100">
         <Ionicons name="logo-twitter" size={24} color="#1DA1F2" />
@@ -26,7 +39,18 @@ const HomeScreen = () => {
         <SignOutButton />
       </View>
       
-      <ScrollView>
+       <ScrollView
+        showsVerticalScrollIndicator={false}
+        className="flex-1"
+        contentContainerStyle={{ paddingBottom: 80 }}
+        refreshControl={
+          <RefreshControl
+            refreshing={isRefetching}
+            onRefresh={handlePullToRefresh}
+            tintColor={"#1DA1F2"}
+          />
+        }
+      >
         <View style={{ backgroundColor: "white", flex: 1 }}>
           <PostComposer />
         </View>
